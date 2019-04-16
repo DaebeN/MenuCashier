5d61b933ed6abdd2574d5851af2a3ab14d1db883
Bayu Alif Febrianta <32250968+DaebeN@users.noreply.github.com>
2019-04-16 22:35:05 +0700

    Update and rename Transaction2.cs to Minuman.cs

---
+++ Minuman.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace responsi
+{
+    public class Minuman
+    {
+        private Dishes _dishes;
+
+        public Minuman()
+        {
+            _dishes = new Dishes();
+        }
+
+        public void PreSales()
+        {
+            Console.WriteLine(_dishes.Info());
+        }
+
+        public void ListDish()
+        {
+            try
+            {
+                if (_dishes.Size() <= 0)
+                {
+                    Console.WriteLine("Tidak ada list makanan");
+                }
+                else
+                {
+                    Console.WriteLine(_dishes.getDish());
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}

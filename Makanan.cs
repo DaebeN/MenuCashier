fa46d97ba1fd16fe6677602bac3334bb610ba57a
Bayu Alif Febrianta <32250968+DaebeN@users.noreply.github.com>
2019-04-16 22:26:58 +0700

    Update and rename Transaction.cs to Makanan.cs
    Percobaan 1

---
+++ Makanan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uts
+{
+    public class Makanan
+    {
+        private Foods _foods;
+
+        public Makanan()
+        {
+        }
+
+        public Makanan(Foods _foods)
+        {
+            this._foods = _foods;
+        }
+
+        public void PreSales()
+        {
+            Console.WriteLine(_foods.Info());
+        }
+    }
+}

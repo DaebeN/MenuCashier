aca90700aae1582189ee6033e44f23fb5451337d
Bayu Alif Febrianta <32250968+DaebeN@users.noreply.github.com>
2019-04-16 22:37:17 +0700

    Update and rename Transaction3.cs to Lainlain.cs

---
+++ Lainlain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace responsi
+{
+    public class Lainlain
+    {
+        public void PreSales(Dishes dishes)
+        {
+            Console.WriteLine(dishes.Info());
+        }
+
+    }
+}
